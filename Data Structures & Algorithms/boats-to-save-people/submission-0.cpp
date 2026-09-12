6e4d2ac64ad9486014519af3fd5e466238a565a5
Akshat Awasthi <113445125+Akshat2113609@users.noreply.github.com>
2026-09-12 18:14:49 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int cnt=0;
+        sort(people.begin(),people.end());
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            int weight = people[i]+people[j];
+            if(weight<=limit){
+                cnt++;
+                people[i]=0;
+                people[j]=0;
+                i++;
+                j--;
+            }
+            else{
+                j--;
+            }
+        }
+        for(auto it:people){
+            if(it!=0 && it<=limit){
+                cnt++;
+            }
+        }
+    return cnt;
+    }
+};
